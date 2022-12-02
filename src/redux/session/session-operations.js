1898d375a3dc38cfaba063a0da208c93b596aa43
Alexander Krasnobokyi <87702429+AlexxRed@users.noreply.github.com>
2022-12-02 19:50:41 +0200

    Update session-operations.js

--- src/redux/session/session-operations.js
+++ src/redux/session/session-operations.js
@@ -3,7 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-axios.defaults.baseURL = 'https://wallet-backend-app-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://wallet-40fe.onrender.com/';
 
 const token = {
   set(token) {
